de9e69b34af0c065c941ba81ec447130b6fd8c66
Rainer Simon <rainer@rainersimon.io>
2024-09-06 10:18:29 +0200

    Reverting broken build

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { Annotorious } from '@annotorious/react';
 import { App } from './App';
 
 import './index.css';
-import '@annotorious/react/annotorious-react.css';
-import '@annotorious/plugin-connectors/annotorious-connectors.css';
 
 const root = createRoot(document.getElementById('app') as Element);
 root.render(
